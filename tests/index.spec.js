bf0a2c3d7a92ed38517c975faf863547b3d655dd
Michael Rojas <michael.rojasp@gmail.com>
2016-11-25 17:16:34 -0600

    Initial repository version.

---
+++ tests/index.spec.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('Our first test',()=>{
+    it('Should pass',()=>{
+      expect(true).toEqual(true);
+    });
+});
---
+++ tools/startMessage.js
@@ -0,0 +1,4 @@
+import colors from 'colors';
+/*eslint-disabled no-console*/
+
+console.log('Starting app in dev mode...'.green);
